41afaf323046c43540fff745d093f6ff68c3d8d1
alsaecas <alejandrosaezc@gmail.com>
2014-02-17 23:19:44 +0100

    Update telegram_info.sh

--- utils/telegram_info.sh
+++ utils/telegram_info.sh
@@ -4,8 +4,8 @@ IP=$(curl http://ipecho.net/plain;)
 IIP=$(/sbin/ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print$
 
 /home/pi/tg/telegram -k /home/pi/tg/tg.pub <<STDIN
-msg user#12278757 Esta es mi IP externa: $IP 
-msg user#12278757 Esta es mi IP interna: $IIP
+msg user#xxxxxxxx Esta es mi IP externa: $IP 
+msg user#xxxxxxxx Esta es mi IP interna: $IIP
 safe_quit
 STDIN
 
